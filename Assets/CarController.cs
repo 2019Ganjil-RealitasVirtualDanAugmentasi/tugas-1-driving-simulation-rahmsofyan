e768d4527c7b65c1b436911524981f3d44c1a2a2
Rahma sofyantoro <rahma.sofyantoro16@mhs.if.its.ac.id>
2019-09-26 23:57:26 +0700

    Controller and Lowo Ireng

---
+++ Assets/CarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputManager))]
+public class CarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputManager IM;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 1f;
+    public float steerCooeffcient = 1f;
+    public float see;
+    void Start()
+    {
+        IM = GetComponent<InputManager>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * IM.throttle;
+        }
+
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = steerCooeffcient *  IM.steer ;
+            see = steerCooeffcient *  IM.steer ;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+tugas-1-driving-simulation-rahmsofyan
+
+#Rahma Sofyantoro
+##05111640000117
